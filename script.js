7f83b1c92be4128b08e5830aad00c5ec98e7149f
AquaJo <84229101+AquaJo@users.noreply.github.com>
2023-01-04 02:45:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.forms['contactForm'].addEventListener('submit', (event) => {
+  event.preventDefault();
+  // TODO do something here to show user that form is being submitted
+  fetch(event.target.action, {
+    method: 'POST',
+    body: new URLSearchParams(new FormData(event.target)) // event.target is the form
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json(); // or response.text() or whatever the server sends
+  }).then((body) => {
+    console.log(body);
+    // TODO handle body
+  }).catch((error) => {
+    console.log(error.message);
+    // TODO handle error
+  });
+});
